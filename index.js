e31b19a716cfd9a075c2ab909aa0b08fcd0b28e7
mfst <123135507+PointMaker1@users.noreply.github.com>
2023-07-23 05:52:10 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+  document.getElementById('autoLoginButton').addEventListener('click', function() {
+    // This function will execute when the "Auto Login" button is clicked.
+    document.getElementById('loginForm').submit();
+  });
+  
